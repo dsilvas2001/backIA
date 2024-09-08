491de2b74b871247a41377ca44d6ceec55395b4b
dsilvas2001 <dsilvas@uteq.edu.ec>
2024-09-08 15:44:16 -0500

    server start

--- dist/app.js
+++ dist/app.js
@@ -1,29 +1,22 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+const data_1 = require("./data");
 const config_1 = require("./config");
-const express_1 = __importDefault(require("express"));
+const presentation_1 = require("./presentation");
+const routes_1 = require("./presentation/routes");
 // (async () => {
 //   await main();
 // })();
-// function main() {
-//   try {
-//     MongoDatabase.connect({
-//       dbName: envs.MONGO_DB_NAME,
-//       mongoURL: envs.MONGO_URL,
-//     });
-//     const server = new Server({
-//       port: envs.PORT,
-//       routes: AppRoutes.routes,
-//     });
-//     server.start();
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-const app = (0, express_1.default)();
-app.listen(config_1.envs.PORT, () => {
-    console.log(`Listen port: ${config_1.envs.PORT}`);
+data_1.MongoDatabase.connect({
+    dbName: config_1.envs.MONGO_DB_NAME,
+    mongoURL: config_1.envs.MONGO_URL,
 });
+const server = new presentation_1.Server({
+    port: config_1.envs.PORT,
+    routes: routes_1.AppRoutes.routes,
+});
+server.start();
+// const app = express();
+// app.listen(envs.PORT, () => {
+//   console.log(`Listen port: ${envs.PORT}`);
+// });
--- src/app.ts
+++ src/app.ts
@@ -9,25 +9,19 @@ import express from "express";
 //   await main();
 // })();
 
-// function main() {
-//   try {
-//     MongoDatabase.connect({
-//       dbName: envs.MONGO_DB_NAME,
-//       mongoURL: envs.MONGO_URL,
-//     });
-
-//     const server = new Server({
-//       port: envs.PORT,
-//       routes: AppRoutes.routes,
-//     });
-
-//     server.start();
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-const app = express();
-app.listen(envs.PORT, () => {
-  console.log(`Listen port: ${envs.PORT}`);
+MongoDatabase.connect({
+  dbName: envs.MONGO_DB_NAME,
+  mongoURL: envs.MONGO_URL,
 });
+
+const server = new Server({
+  port: envs.PORT,
+  routes: AppRoutes.routes,
+});
+
+server.start();
+
+// const app = express();
+// app.listen(envs.PORT, () => {
+//   console.log(`Listen port: ${envs.PORT}`);
+// });
